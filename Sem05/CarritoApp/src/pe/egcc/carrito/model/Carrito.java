75fff453f776456b9543e894f22d8bef79e74c50
Gustavo Coronel <gcoronelc@gmail.com>
2017-09-12 16:29:09 -0500

    Sem 05

---
+++ Sem05/CarritoApp/src/pe/egcc/carrito/model/Carrito.java
@@ -0,0 +1,54 @@
+package pe.egcc.carrito.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Carrito {
+  
+  private List<Item> lista;
+  private double importe;
+  private double impuesto;
+  private double total;
+  
+  public Carrito() {
+    lista = new ArrayList<>();
+  }
+  
+  public List<Item> getLista() {
+    return lista;
+  }
+  
+  public double getImporte() {
+    return importe;
+  }
+  
+  public double getImpuesto() {
+    return impuesto;
+  }
+  
+  public double getTotal() {
+    return total;
+  }
+
+  public void agregar(Item item){
+    lista.add(item);
+    ordenarIds();
+    calcularTotal();
+  }
+
+  private void calcularTotal() {
+    total = 0.0;
+    for(Item item: lista){
+      total += item.getSubtotal();
+    }
+    importe = total / 1.18;
+    impuesto = total - importe;
+  }
+
+  private void ordenarIds() {
+    for(int i=0; i < lista.size(); i++){
+      lista.get(i).setId(i + 1);
+    }
+  }
+  
+}
---
+++ Sem05/CarritoApp/src/pe/egcc/carrito/prueba/Prueba01.java
@@ -0,0 +1,30 @@
+package pe.egcc.carrito.prueba;
+
+import pe.egcc.carrito.model.Carrito;
+import pe.egcc.carrito.model.Item;
+
+public class Prueba01 {
+
+  public static void main(String[] args) {
+    
+    // Carrito
+    Carrito carrito = new Carrito();
+    
+    // Items
+    carrito.agregar(new Item("Producto 1", 100, 5));
+    carrito.agregar(new Item("Producto 2", 90, 8));
+    carrito.agregar(new Item("Producto 3", 135, 7));
+    
+    // Reporte
+    for(Item item: carrito.getLista()){
+      System.out.println(item.getId() + " | " + item.getProducto() 
+        + " | " + item.getPrecio() + " | " + item.getCant() + " | " + item.getSubtotal());
+    }
+    System.out.println("Importe: " + carrito.getImporte());
+    System.out.println("Impuesto: " + carrito.getImpuesto());
+    System.out.println("Total: " + carrito.getTotal());
+    
+  }
+  
+  
+}
