ff683cbeca23f562c634d977688c5f0dd7ba8de1
Gustavo Coronel <gcoronelc@gmail.com>
2017-10-10 16:28:00 -0500

    Sem 09

---
+++ Sem09/CondominioApp/src/pe/egcc/condominio/service/LogonService.java
@@ -0,0 +1,16 @@
+package pe.egcc.condominio.service;
+
+import org.springframework.stereotype.Service;
+
+import pe.egcc.condominio.model.Persona;
+
+@Service
+public class LogonService {
+
+  public Persona validarUsuario(String usuario, String clave){
+    Persona bean = null;
+    
+    return bean;
+  }
+  
+}
